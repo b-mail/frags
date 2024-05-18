c4797301b1bcc4711614fa09a72afe66037857bc
b-mail <bswoo04@gmail.com>
2024-05-18 22:13:41 +0900

    :lipstick: 내비게이션 바가 로딩 모달창보다 위에 있도록 수정

--- components/ui/LoadingModal.tsx
+++ components/ui/LoadingModal.tsx
@@ -2,7 +2,7 @@ import LoadingIndicator from "@/components/ui/LoadingIndicator";
 
 export default function LoadingModal({ message }: { message: string }) {
   return (
-    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-slate-900 bg-opacity-30 backdrop-blur">
+    <div className="fixed left-0 top-0 z-40 flex h-screen w-screen items-center justify-center bg-slate-900 bg-opacity-30 backdrop-blur">
       <LoadingIndicator message={message} />
     </div>
   );
